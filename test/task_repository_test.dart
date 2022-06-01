19db65779a66355cf654569799dcfafd70e3a211
Trần Anh Tú <64175794+Tu98-math@users.noreply.github.com>
2022-06-02 04:07:43 +0700

    update yaml

--- test/task_repository_test.dart
+++ test/task_repository_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 import 'package:to_do_manabie/model/task_model.dart';
 import 'package:to_do_manabie/repository/task_repository.dart';
 
